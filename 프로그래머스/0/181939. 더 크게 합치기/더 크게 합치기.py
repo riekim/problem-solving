be723a1f167db56a55f945f862af7181c7be675f
kimire <rladlfp888@gmail.com>
2026-05-19 19:01:16 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,13 +1,17 @@
 def solution(a, b):
     answer = 0
     
+    # 두 정수를 문자열로 붙인 뒤, 다시 int()로 감싸 숫자로 변환한 후 변수에 저장
     ab = int(str(a) + str(b))
     ba = int(str(b) + str(a))
     
+    # a ⊕ b가 더 큰 경우
     if ab > ba:
         answer = ab
+    # b ⊕ a가 더 큰 경우
     elif ab < ba:
         answer = ba
+    # 두 값이 동일한 경우 (a ⊕ b rerturn)
     else:
         answer = ab
         
