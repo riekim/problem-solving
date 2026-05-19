a5ff305a802ec5bfbb98f4a43bb8d71a255a8abb
kimire <rladlfp888@gmail.com>
2026-05-19 19:47:08 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,12 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if (ineq, eq) == (">", "="):
+        answer = int(n >= m)
+    elif (ineq, eq) == ("<", "="):
+        answer = int(n <= m)
+    elif (ineq, eq) == (">", "!"):
+        answer = int(n > m)
+    else:
+        answer = int(n < m)
+            
+    return answer
